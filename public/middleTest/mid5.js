d18f10bafb10708e35e1b75640d72abe82639d35
Lee, youngsun <dlsnsiti@gmail.com>
2020-05-04 10:00:47 +0900

    중간고사 3번째 커밋

--- public/middleTest/mid5.js
+++ public/middleTest/mid5.js
@@ -3,3 +3,12 @@ let s = [];
 for(let i = 0 ; i < 100 ; i++)
     s[i] = Math.floor(Math.random() * 100 + 1); // 1 이상 100 이하
 
+let array = s.filter((e) => e % 2 === 0 );
+
+let sum = array.reduce(callback)
+
+console.log(sum)
+
+function callback(a, b) {
+    return a + b;
+}
